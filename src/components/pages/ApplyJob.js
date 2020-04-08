8641198e0251fd1fff3f44968eede68a83613126
Saptarshi Misra <saptarshi.misra@gmail.com>
2020-04-08 16:04:49 +0530

    changes payload to apply and code to handle

--- src/components/pages/ApplyJob.js
+++ src/components/pages/ApplyJob.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import { Redirect } from 'react-router'
 
 export const ApplyJob = (props) => {
-    return (
-        <div>
-            <h1> Applying for Job </h1>
+    if(!props.location.job)
+    {
+        return <Redirect to="/" />
+    }else{
+        let data  = props.location.job;
+        let jobDetail = '';
+        let salary = '';
+        let location = '';
+        if(data){
+            jobDetail = data[0].titleDesc;
+            salary = data[2].titleDesc;
+            location = data[1].titleDesc;
+        }
+        return (
             <div>
-                Job Detail: {props.location.job.titleDesc}
+                <h1> Going to Apply for the below job</h1>
+                <hr/>
+                <div>
+                    <div>Job Detail: {jobDetail}</div>
+                    <div>{salary}</div>
+                    <div>Location: {location}</div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
