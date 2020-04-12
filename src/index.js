cf0c06038e13e862c23f59f239da304d3638f129
Saptarshi Misra <saptarshi.misra@gmail.com>
2020-04-12 23:55:15 +0530

    changes for cognito integration

--- src/index.js
+++ src/index.js
@@ -10,8 +10,43 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import allReducers from './reducers';
 
+import { Amplify } from 'aws-amplify';
+import {AWS} from './config';
+
+
 const store = createStore(allReducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: AWS.cognito.REGION,
+    userPoolId: AWS.cognito.USER_POOL_ID,
+    identityPoolId: AWS.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: AWS.cognito.APP_CLIENT_ID,
+    oauth: {
+      domain: 'retailjobs.auth.ap-south-1.amazoncognito.com',
+      scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+      redirectSignIn: 'https://84525b01.ngrok.io',
+      redirectSignOut: 'https://84525b01.ngrok.io',
+      responseType: 'token' // or 'token', note that REFRESH token will only be generated when the responseType is code
+  }
+  },
+  Storage: {
+    region: AWS.s3.REGION,
+    bucket: AWS.s3.BUCKET,
+    identityPoolId: AWS.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "retailjobs",
+        endpoint: AWS.apiGateway.URL,
+        region: AWS.apiGateway.REGION
+      },
+    ]
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
